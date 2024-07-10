95aac488688f4dae8df9b5855a989cd179db5169
Hanefi <hanefigurler@gmail.com>
2024-07-10 12:45:39 +0200

    Lesson7 homework is created

---
+++ Lesson7-homework/index.js
@@ -0,0 +1,69 @@
+function getElements() {
+  const input = document.getElementById('location')
+  const button = document.getElementById('submitButton')
+  const weatherForecast = document.getElementById('weatherForecast')
+
+
+  return {
+    input: input,
+    button: button,
+    weatherForecast: weatherForecast,
+  }
+}
+
+const weatherList = []
+
+function getInputValue() {
+  const elements = getElements()
+  if (elements.input !== null) {
+    weatherList.length = 0
+    document.getElementById('weatherForecast').innerHTML = ''
+    const value = elements.input.value
+    console.log('Getting coordinates for ' + value)
+    fetchCoordinates(value)
+  }
+}
+
+async function fetchCoordinates(item) {
+  console.log('fetching...')
+  const url = 'https://geocode.maps.co/search?q=' + item + '&api_key=6633b73ddf675448401146nfs481903'
+  const response = await fetch(url)
+  const data = await response.json()
+  
+  const firstItem = data[0]
+  const lat = firstItem.lat
+  const lon = firstItem.lon
+  console.log('Latitude:' + lat + ', Longitude:' + lon)
+
+  getWeatherForecast(lat,lon)
+}
+
+async function getWeatherForecast(lat, lon) {
+  console.log('Pulling clouds...')
+  const url = 'https://api.open-meteo.com/v1/forecast?latitude=' + lat + '&longitude=' + lon + '&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin'
+  const weather = await fetch(url)
+  const data = await weather.json()
+  
+  console.log(data)
+  showWeatherForecast(data)
+}
+
+function showWeatherForecast(data) {
+  const elements = getElements()
+
+  for (let i = 0; i <= 6; i++){
+    const date = data.daily.time[i]
+    console.log(date)
+    const minTemp = data.daily.temperature_2m_min[i]
+    console.log(minTemp)
+    const maxTemp = data.daily.temperature_2m_max[i]
+    console.log(maxTemp)
+    const concatDayandTemp = date + minTemp + maxTemp
+    weatherList.push(concatDayandTemp)
+
+    const li = document.createElement('li')
+
+    li.append(concatDayandTemp)
+    elements.weatherForecast.append(li)
+  }
+}
